6b7ac55b7fec905e0e3bdd7efa378fe61fdb573c
gra gra pow <155625311+unoxyzmdfckz@users.noreply.github.com>
2025-05-14 17:42:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -356,4 +356,5 @@ if __name__ == '__main__':
         logger.error(f"You must specify a {COLORS.get('YELLOW')}User-Agent{COLORS.get('RESET')} for Turnstile Solver or use {COLORS.get('GREEN')}camoufox{COLORS.get('RESET')} without useragent")
     else:
         app = create_app(headless=args.headless, debug=args.debug, useragent=args.useragent, browser_type=args.browser_type, thread=args.thread, proxy_support=args.proxy)
-        app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+
